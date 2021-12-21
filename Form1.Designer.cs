1c466febd544837e9a95dd92854354f30dd84367
WillSon <WillSon@DESKTOP-407MDTA>
2021-12-21 13:16:45 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace LD08_tester
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 필수 디자이너 변수입니다.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 사용 중인 모든 리소스를 정리합니다.
+    /// </summary>
+    /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form 디자이너에서 생성한 코드
+
+    /// <summary>
+    /// 디자이너 지원에 필요한 메서드입니다. 
+    /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.label1 = new System.Windows.Forms.Label();
+      this.serialComboBox = new System.Windows.Forms.ComboBox();
+      this.button_open = new System.Windows.Forms.Button();
+      this.button_close = new System.Windows.Forms.Button();
+      this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.textBox_time_gap = new System.Windows.Forms.TextBox();
+      this.label8 = new System.Windows.Forms.Label();
+      this.textBox_angle_gap = new System.Windows.Forms.TextBox();
+      this.label7 = new System.Windows.Forms.Label();
+      this.textBox_data_length = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.textBox_time = new System.Windows.Forms.TextBox();
+      this.textBox_end_angle = new System.Windows.Forms.TextBox();
+      this.textBox_start_angle = new System.Windows.Forms.TextBox();
+      this.textBox_rpm = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.textBox = new System.Windows.Forms.TextBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(69, 12);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "시리얼 포트";
+      // 
+      // serialComboBox
+      // 
+      this.serialComboBox.FormattingEnabled = true;
+      this.serialComboBox.Location = new System.Drawing.Point(100, 6);
+      this.serialComboBox.Name = "serialComboBox";
+      this.serialComboBox.Size = new System.Drawing.Size(140, 20);
+      this.serialComboBox.TabIndex = 1;
+      // 
+      // button_open
+      // 
+      this.button_open.Location = new System.Drawing.Point(100, 58);
+      this.button_open.Name = "button_open";
+      this.button_open.Size = new System.Drawing.Size(127, 23);
+      this.button_open.TabIndex = 3;
+      this.button_open.Text = "포트 열기 [Enter]";
+      this.button_open.UseVisualStyleBackColor = true;
+      this.button_open.Click += new System.EventHandler(this.button_open_Click);
+      // 
+      // button_close
+      // 
+      this.button_close.Location = new System.Drawing.Point(100, 96);
+      this.button_close.Name = "button_close";
+      this.button_close.Size = new System.Drawing.Size(127, 23);
+      this.button_close.TabIndex = 4;
+      this.button_close.Text = "포트 닫기 [ESC]";
+      this.button_close.UseVisualStyleBackColor = true;
+      this.button_close.Click += new System.EventHandler(this.button_close_Click);
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.textBox_time_gap);
+      this.groupBox1.Controls.Add(this.label8);
+      this.groupBox1.Controls.Add(this.textBox_angle_gap);
+      this.groupBox1.Controls.Add(this.label7);
+      this.groupBox1.Controls.Add(this.textBox_data_length);
+      this.groupBox1.Controls.Add(this.label6);
+      this.groupBox1.Controls.Add(this.textBox_time);
+      this.groupBox1.Controls.Add(this.textBox_end_angle);
+      this.groupBox1.Controls.Add(this.textBox_start_angle);
+      this.groupBox1.Controls.Add(this.textBox_rpm);
+      this.groupBox1.Controls.Add(this.label5);
+      this.groupBox1.Controls.Add(this.label4);
+      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.Controls.Add(this.label2);
+      this.groupBox1.Location = new System.Drawing.Point(14, 155);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(260, 242);
+      this.groupBox1.TabIndex = 5;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "LDS RAW Data";
+      // 
+      // textBox_time_gap
+      // 
+      this.textBox_time_gap.Location = new System.Drawing.Point(86, 204);
+      this.textBox_time_gap.Name = "textBox_time_gap";
+      this.textBox_time_gap.Size = new System.Drawing.Size(140, 21);
+      this.textBox_time_gap.TabIndex = 13;
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(19, 213);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(61, 12);
+      this.label8.TabIndex = 12;
+      this.label8.Text = "Time Gap";
+      // 
+      // textBox_angle_gap
+      // 
+      this.textBox_angle_gap.Location = new System.Drawing.Point(86, 177);
+      this.textBox_angle_gap.Name = "textBox_angle_gap";
+      this.textBox_angle_gap.Size = new System.Drawing.Size(140, 21);
+      this.textBox_angle_gap.TabIndex = 11;
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(16, 186);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(64, 12);
+      this.label7.TabIndex = 10;
+      this.label7.Text = "Angle Gap";
+      // 
+      // textBox_data_length
+      // 
+      this.textBox_data_length.Location = new System.Drawing.Point(86, 20);
+      this.textBox_data_length.Name = "textBox_data_length";
+      this.textBox_data_length.Size = new System.Drawing.Size(140, 21);
+      this.textBox_data_length.TabIndex = 9;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(8, 29);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(72, 12);
+      this.label6.TabIndex = 8;
+      this.label6.Text = "Data Length";
+      // 
+      // textBox_time
+      // 
+      this.textBox_time.Location = new System.Drawing.Point(86, 150);
+      this.textBox_time.Name = "textBox_time";
+      this.textBox_time.Size = new System.Drawing.Size(140, 21);
+      this.textBox_time.TabIndex = 7;
+      // 
+      // textBox_end_angle
+      // 
+      this.textBox_end_angle.Location = new System.Drawing.Point(86, 117);
+      this.textBox_end_angle.Name = "textBox_end_angle";
+      this.textBox_end_angle.Size = new System.Drawing.Size(140, 21);
+      this.textBox_end_angle.TabIndex = 6;
+      // 
+      // textBox_start_angle
+      // 
+      this.textBox_start_angle.Location = new System.Drawing.Point(86, 84);
+      this.textBox_start_angle.Name = "textBox_start_angle";
+      this.textBox_start_angle.Size = new System.Drawing.Size(140, 21);
+      this.textBox_start_angle.TabIndex = 5;
+      // 
+      // textBox_rpm
+      // 
+      this.textBox_rpm.Location = new System.Drawing.Point(86, 51);
+      this.textBox_rpm.Name = "textBox_rpm";
+      this.textBox_rpm.Size = new System.Drawing.Size(140, 21);
+      this.textBox_rpm.TabIndex = 4;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(46, 159);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(34, 12);
+      this.label5.TabIndex = 3;
+      this.label5.Text = "Time";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(17, 126);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(63, 12);
+      this.label4.TabIndex = 2;
+      this.label4.Text = "End Angle";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(14, 93);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(66, 12);
+      this.label3.TabIndex = 1;
+      this.label3.Text = "Start Angle";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(48, 60);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(32, 12);
+      this.label2.TabIndex = 0;
+      this.label2.Text = "RPM";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.textBox);
+      this.groupBox2.Location = new System.Drawing.Point(280, 239);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(423, 158);
+      this.groupBox2.TabIndex = 14;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Status Message";
+      // 
+      // textBox
+      // 
+      this.textBox.Location = new System.Drawing.Point(16, 27);
+      this.textBox.Multiline = true;
+      this.textBox.Name = "textBox";
+      this.textBox.Size = new System.Drawing.Size(388, 114);
+      this.textBox.TabIndex = 15;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(280, 9);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(423, 218);
+      this.pictureBox1.TabIndex = 15;
+      this.pictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(718, 409);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.button_close);
+      this.Controls.Add(this.button_open);
+      this.Controls.Add(this.serialComboBox);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox serialComboBox;
+    private System.Windows.Forms.Button button_open;
+    private System.Windows.Forms.Button button_close;
+    private System.IO.Ports.SerialPort serialPort1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.TextBox textBox_time;
+    private System.Windows.Forms.TextBox textBox_end_angle;
+    private System.Windows.Forms.TextBox textBox_start_angle;
+    private System.Windows.Forms.TextBox textBox_rpm;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox_data_length;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.TextBox textBox_time_gap;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.TextBox textBox_angle_gap;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.TextBox textBox;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LD08_tester
+{
+  public partial class Form1 : Form
+  {
+    private Thread readThread;
+    private Thread processThread;
+    //private Thread drawThread;
+
+    private int data_length = 0;
+    private int lds_rpm = 0;
+    private double start_angle = 0;
+    private double end_angle = 0;
+    private int timestamp = 0;
+    private int timestamp_temp = 0;
+
+    private bool _continue = true;
+    private int[] raw_data = new int[100];
+    private int[] parse_data = new int[47];
+    private int write_index = 0;
+    private int read_index = 0;
+    private byte[] CrcTable = new byte[256] {
+      0x00, 0x4d, 0x9a, 0xd7, 0x79, 0x34, 0xe3,
+      0xae, 0xf2, 0xbf, 0x68, 0x25, 0x8b, 0xc6, 0x11, 0x5c, 0xa9, 0xe4, 0x33,
+      0x7e, 0xd0, 0x9d, 0x4a, 0x07, 0x5b, 0x16, 0xc1, 0x8c, 0x22, 0x6f, 0xb8,
+      0xf5, 0x1f, 0x52, 0x85, 0xc8, 0x66, 0x2b, 0xfc, 0xb1, 0xed, 0xa0, 0x77,
+      0x3a, 0x94, 0xd9, 0x0e, 0x43, 0xb6, 0xfb, 0x2c, 0x61, 0xcf, 0x82, 0x55,
+      0x18, 0x44, 0x09, 0xde, 0x93, 0x3d, 0x70, 0xa7, 0xea, 0x3e, 0x73, 0xa4,
+      0xe9, 0x47, 0x0a, 0xdd, 0x90, 0xcc, 0x81, 0x56, 0x1b, 0xb5, 0xf8, 0x2f,
+      0x62, 0x97, 0xda, 0x0d, 0x40, 0xee, 0xa3, 0x74, 0x39, 0x65, 0x28, 0xff,
+      0xb2, 0x1c, 0x51, 0x86, 0xcb, 0x21, 0x6c, 0xbb, 0xf6, 0x58, 0x15, 0xc2,
+      0x8f, 0xd3, 0x9e, 0x49, 0x04, 0xaa, 0xe7, 0x30, 0x7d, 0x88, 0xc5, 0x12,
+      0x5f, 0xf1, 0xbc, 0x6b, 0x26, 0x7a, 0x37, 0xe0, 0xad, 0x03, 0x4e, 0x99,
+      0xd4, 0x7c, 0x31, 0xe6, 0xab, 0x05, 0x48, 0x9f, 0xd2, 0x8e, 0xc3, 0x14,
+      0x59, 0xf7, 0xba, 0x6d, 0x20, 0xd5, 0x98, 0x4f, 0x02, 0xac, 0xe1, 0x36,
+      0x7b, 0x27, 0x6a, 0xbd, 0xf0, 0x5e, 0x13, 0xc4, 0x89, 0x63, 0x2e, 0xf9,
+      0xb4, 0x1a, 0x57, 0x80, 0xcd, 0x91, 0xdc, 0x0b, 0x46, 0xe8, 0xa5, 0x72,
+      0x3f, 0xca, 0x87, 0x50, 0x1d, 0xb3, 0xfe, 0x29, 0x64, 0x38, 0x75, 0xa2,
+      0xef, 0x41, 0x0c, 0xdb, 0x96, 0x42, 0x0f, 0xd8, 0x95, 0x3b, 0x76, 0xa1,
+      0xec, 0xb0, 0xfd, 0x2a, 0x67, 0xc9, 0x84, 0x53, 0x1e, 0xeb, 0xa6, 0x71,
+      0x3c, 0x92, 0xdf, 0x08, 0x45, 0x19, 0x54, 0x83, 0xce, 0x60, 0x2d, 0xfa,
+      0xb7, 0x5d, 0x10, 0xc7, 0x8a, 0x24, 0x69, 0xbe, 0xf3, 0xaf, 0xe2, 0x35,
+      0x78, 0xd6, 0x9b, 0x4c, 0x01, 0xf4, 0xb9, 0x6e, 0x23, 0x8d, 0xc0, 0x17,
+      0x5a, 0x06, 0x4b, 0x9c, 0xd1, 0x7f, 0x32, 0xe5, 0xa8
+    };
+
+    public Form1()
+    {
+      InitializeComponent();
+
+      //drawThread = new Thread(drawLDS);
+      //drawThread.Start();
+
+      string[] portNames = SerialPort.GetPortNames();
+      foreach (string portnumber in portNames)
+      {
+        serialComboBox.Items.Add(portnumber);
+        serialComboBox.SelectedIndex = 0;
+      }
+
+      if (portNames.Length != 0)
+      {
+        button_open.Enabled = true;
+      }
+      else
+      {
+        button_open.Enabled = false;
+      }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      this.KeyPreview = true;
+    }
+
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Enter)
+      {
+        button_open.PerformClick();
+      }
+      if (e.KeyCode == Keys.Escape)
+      {
+        button_close.PerformClick();
+      }
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (processThread != null && processThread.IsAlive)
+      {
+        processThread.Abort();
+      }
+      if (readThread != null && readThread.IsAlive)
+      {
+        readThread.Abort();
+      }
+    }
+
+    private void button_open_Click(object sender, EventArgs e)
+    {
+      if (serialPort1.IsOpen == false)
+      {
+        _continue = true;
+        Array.Clear(raw_data, 0, raw_data.Length);
+
+        serialPort1.PortName = serialComboBox.SelectedItem.ToString();
+        serialPort1.BaudRate = 115200;
+        serialPort1.DataBits = 8;
+        serialPort1.StopBits = StopBits.One;
+        serialPort1.Parity = Parity.None;
+        serialPort1.Open();
+
+        textBox.Text = Environment.NewLine + "포트가 열렸습니다." + Environment.NewLine;
+
+        readThread = new Thread(Read);
+        readThread.Start();
+        processThread = new Thread(Process);
+        processThread.Start();
+      }
+      else
+      {
+        textBox.Text = Environment.NewLine + "포트가 이미 열려있습니다." + Environment.NewLine;
+      }
+    }
+
+    private void button_close_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        if (serialPort1.IsOpen)
+        {
+          Array.Clear(raw_data, 0, raw_data.Length);
+          serialPort1.DiscardInBuffer();
+          serialPort1.DiscardOutBuffer();
+
+          textBox.Text = Environment.NewLine + "포트를 닫았습니다." + Environment.NewLine;
+
+          _continue = false;
+        }
+        else
+        {
+          textBox.Text = Environment.NewLine + "포트가 이미 닫혀있습니다." + Environment.NewLine;
+        }
+      }
+      catch (Exception ex)
+      {
+        //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+    }
+
+    private void Read()
+    {
+      while (_continue)
+      {
+        try
+        {
+          int rx_data = serialPort1.ReadByte();
+          raw_data[write_index++] = rx_data;
+          if (write_index >= 100)
+          {
+            write_index = 0;
+          }
+
+          // print received data on the textbox
+          //this.Invoke((MethodInvoker)delegate
+          //{
+          //  textBox.AppendText(string.Format("{0:X2} ", rx_data));
+          //});
+        } catch (Exception ex)
+        {
+          //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+
+      if (_continue == false)
+      {
+        try
+        {
+          serialPort1.Close();
+          readThread.Join();
+          readThread.Abort();
+        }
+        catch (Exception ex)
+        {
+          //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+    }
+
+    private void Process()
+    {
+      int temp_index = 0;
+      while (_continue)
+      {
+        if (raw_data[read_index] == 0x54)
+        {
+          parse_data[temp_index] = raw_data[read_index];
+          temp_index = temp_index + 1;
+          read_index = read_index + 1;
+          if (read_index >= 100)
+          {
+            read_index = 0;
+          }
+          if (raw_data[read_index] == 0x2C)
+          {
+            for (int count = 0; count <46; count++)
+            {
+              parse_data[temp_index] = raw_data[read_index];
+              temp_index = temp_index + 1;
+              if (temp_index > 47)
+                temp_index = 0;
+              read_index = read_index + 1;
+              if(read_index >= 100)
+              {
+                read_index = 0;
+              }
+            }
+            if(!checkCRC())
+            {
+              // CRC ERROR!!!
+              this.Invoke((MethodInvoker)delegate
+              {
+                textBox.Text = "[ERROR] Invalid CRC!!!";
+              });
+              //Array.Clear(raw_data, 0, raw_data.Length);
+              //read_index = 0;
+              read_index = 0;
+              temp_index = 0;
+              Array.Clear(parse_data, 0, parse_data.Length);
+            }
+            this.Invoke((MethodInvoker)delegate
+            {
+              textBox_rpm.Text = lds_rpm.ToString();
+              textBox_data_length.Text = data_length.ToString();
+              textBox_start_angle.Text = start_angle.ToString();
+              textBox_end_angle.Text = end_angle.ToString();
+              textBox_time.Text = timestamp.ToString();
+              textBox_angle_gap.Text = (end_angle - start_angle).ToString();
+              //textBox_time_gap.Text = (timestamp - timestamp_temp).ToString();
+              textBox_time_gap.Text = timestamp_temp.ToString();
+            });
+          }
+          else
+          {
+            read_index = read_index + 1;
+            temp_index = 0;
+            if (read_index >= 100)
+            {
+              read_index = 0;
+            }
+          }
+        }
+        else
+        {
+          read_index = read_index + 1;
+          temp_index = 0;
+          if (read_index >= 100)
+          {
+            read_index = 0;
+          }
+        }
+      }
+
+      if (_continue == false)
+      {
+        try
+        {
+          processThread.Join();
+          processThread.Abort();
+        }
+        catch (Exception ex)
+        {
+          //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+    }
+
+    private bool checkCRC()
+    {
+      int crc = 0;
+      int i = 0;
+
+      //this.Invoke((MethodInvoker)delegate
+      //{
+      //  textBox.Text += Environment.NewLine + "==================" + Environment.NewLine;
+      //});
+
+      for (i = 0; i < 47; i++)
+      {
+        crc = CrcTable[(crc ^ parse_data[i]) & 0xFF];
+        this.Invoke((MethodInvoker)delegate
+        {
+          textBox.Text = textBox.Text + string.Format("{0:X2} ", parse_data[i]);
+        });
+      }
+      this.Invoke((MethodInvoker)delegate
+      {
+        textBox.Text = textBox.Text + Environment.NewLine + "Cal CRC : " + string.Format("{0:X2} ", crc) + ", CRC : " + string.Format("{0:X2} ", parse_data[46]) + Environment.NewLine;
+      });
+      if (crc == parse_data[46])
+      {
+        data_length = parse_data[1];
+        lds_rpm = (parse_data[3] << 8) + parse_data[2];
+        start_angle = ((parse_data[5] << 8) + parse_data[4]) / 100.0;
+        end_angle = ((parse_data[43] << 8) + parse_data[42]) / 100.0;
+        timestamp = (parse_data[45] << 8) + parse_data[44];
+        timestamp_temp = timestamp;
+        return true;
+      }
+      else
+      {
+        // CRC ERROR!!!
+        this.Invoke((MethodInvoker)delegate
+        {
+          textBox.Text += string.Format("[ERROR] Invalid calc CRC: {0:X2}", crc) + Environment.NewLine;
+        });
+        return false;
+      }
+    }
+
+    //private void drawLDS()
+    //{
+    //  System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+    //  System.Drawing.Graphics formGraphics;
+    //  formGraphics = this.CreateGraphics();
+    //  formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+    //  myBrush.Dispose();
+    //  formGraphics.Dispose();
+    //}
+  }
+}
